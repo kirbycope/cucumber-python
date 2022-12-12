4162fb1e3dca23f9dab2bb6efe5d37a0223114b2
Timothy Cope <kirbycope@gmail.com>
2022-12-12 14:58:50 -0800

    Update common_steps.py

--- appium_webdriver/features/steps/common_steps.py
+++ appium_webdriver/features/steps/common_steps.py
@@ -5,5 +5,5 @@ from selenium.webdriver.common.by import By
 # Then I should see a message saying <message>
 @then('I should see a message saying {message}')
 def step_impl(context, message):
-    element = test_data.driver.find_element(By.XPATH"//*[@text='" + message + "']")
+    element = test_data.driver.find_element(By.XPATH, "//*[@text='" + message + "']")
     assert element.is_displayed()
